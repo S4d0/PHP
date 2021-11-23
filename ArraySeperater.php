86f68005b56239a58dea2c3fa6d26b0ea3ae4ebb
S4d0 <88021410+S4d0@users.noreply.github.com>
2021-11-23 19:59:59 +0300

    Add files via upload

---
+++ ArraySeperater.php
@@ -0,0 +1,22 @@
+<?php
+
+$str = "S4d0!i dont like%P H P= i hate ]HTML&test_sEt";
+$specialChr = array("!","+","%","&","/","(",")","=","?","_","-","}","]","[","{","$","#","}");
+$arr = array();
+foreach ($specialChr as $chr)
+{
+    if(strpos($str,$chr) != null)
+    {
+        array_push($arr, strpos($str,$chr));
+    }
+}
+sort($arr);
+
+$start = 0; 
+for($i=0; $i<count($arr); $i++)
+{
+    echo "<br>" . substr($str,$start,($arr[$i]-$start));
+    $start = $arr[$i]+1;
+}
+
+?>
